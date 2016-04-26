51262af57a71697c31e39ba49d74c0e621be2d80
ian asaff <ian.asaff@gmail.com>
2016-04-26 14:17:28 -0400

    more refactor

--- git_integrate/lib/git_integrate.ex
+++ git_integrate/lib/git_integrate.ex
@@ -4,14 +4,39 @@ defmodule GitIntegrate do
   @integration_file ".integration_branches.json"
 
   def main(args) do
-    base_branch = "develop"
-    integration_branch_name = "integration-branch-#{:erlang.system_time}"
-    branches = File.read!(@integration_file)
-            |> Poison.Parser.parse!
-            |> Map.get("branches")
-    create_integration_branch(base_branch, integration_branch_name)
-    Enum.each(branches, &merge_branch/1)
-    IO.puts "Created integration branch: #{integration_branch_name}"
+    base = "develop"
+    integration = get_integration_branch
+    create_integration_branch(base, integration)
+    get_branches |> merge_branches
+    IO.puts "Built integration branch: #{integration}"
+  end
+
+  def get_integration_branch do
+    suffix = :erlang.system_time
+              |> to_string
+              |> String.slice(-6..-1)
+    "integration-branch-#{suffix}"
+  end
+
+  def create_integration_branch(base, integration) do
+    git_cmd(~w(checkout #{base}))
+    git_cmd(~w(checkout -b #{integration}))
+  end
+
+  def get_branches do
+    File.read!(@integration_file)
+    |> Poison.Parser.parse!
+    |> Map.get("branches")
+  end
+
+  def git_cmd(cmds) do
+    System.cmd("git", cmds) |> check_exit_status
+  end
+
+  def merge_branches(branches) do
+    Enum.each(branches, fn branch ->
+      git_cmd(~w(merge #{branch}))
+    end)
   end
 
   def check_exit_status({output, exit_status}) do
@@ -19,18 +44,5 @@ defmodule GitIntegrate do
       exit({:shutdown, exit_status})
     end
     {output, exit_status}
-  end
-
-  def create_integration_branch(base_branch_name, integration_branch_name) do
-    git_cmd(~w(checkout #{base_branch_name}))
-    git_cmd(~w(checkout -b #{integration_branch_name}))
-  end
-
-  def git_cmd(cmds) do
-    System.cmd("git", cmds) |> check_exit_status
-  end
-
-  def merge_branch(branch_to_merge) do
-    git_cmd(~w(merge #{branch_to_merge}))
   end
 end
