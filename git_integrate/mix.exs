83a99a2359940ef725aaf932524f0ab95c8c3427
ian asaff <ian.asaff@gmail.com>
2016-07-19 15:59:42 -0400

    upgrade to elixir 1.3.2

--- git_integrate/mix.exs
+++ git_integrate/mix.exs
@@ -4,7 +4,7 @@ defmodule GitIntegrate.Mixfile do
   def project do
     [app: :git_integrate,
       version: "0.0.1",
-      elixir: "~> 1.2",
+      elixir: "~> 1.3.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps,
